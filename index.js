7f309b8f9550937e1086eadb27f0fb6a2363f03b
Ren <45683120+renCoder1@users.noreply.github.com>
2019-01-18 21:53:13 -0800

    dice-roll1.0

---
+++ index.js
@@ -0,0 +1,31 @@
+alert("hello");
+
+function onRollClick() {
+  var randomDiceNumber1 = Math.floor(Math.random() * 6 + 1);
+  var randomDiceNumber2 = Math.floor(Math.random() * 6 + 1);
+
+  var redDiceImg = randomDiceNumber1 + ".png";
+  var blackDiceImg = randomDiceNumber2 + ".png";
+
+  var redDice = document.getElementsByClassName('diceRed');
+  var blackDice = document.getElementsByClassName('diceBlack');
+
+  redDice[0].setAttribute("src", "images/" + redDiceImg);
+  blackDice[0].setAttribute("src", "images/black" + blackDiceImg);
+
+if(randomDiceNumber1 > randomDiceNumber2){
+  document.getElementsByClassName("result_display_h2")[0].innerText= "Player 1 Wins!";
+  document.getElementsByClassName("result-img")[0].setAttribute("src","images/redWin.png");
+}
+else if(randomDiceNumber1 === randomDiceNumber2){
+  document.getElementsByClassName("result_display_h2")[0].innerText = "Draws!";
+  document.getElementsByClassName("result-img")[0].setAttribute("src","images/draws.png");
+}
+else
+  {
+    document.getElementsByClassName("result_display_h2")[0].innerText = "Player 2 Wins";
+
+    document.getElementsByClassName("result-img")[0].setAttribute("src","images/blackWin.png");
+  }
+
+}
